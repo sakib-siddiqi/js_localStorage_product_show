e58471a5bd845e5944a251cf358411bc29864213
sakibsiddiqi15 <sakibsiddiqi15@gmail.com>
2021-09-07 15:10:16 +0600

    ternary operator

--- app.js
+++ app.js
@@ -17,31 +17,23 @@ function addToLS() {
   let countVal = parseInt(count.value);
   if (nameVal.length !== 0) {
     let singleProduct = { name: nameVal, count: countVal };
-    if (
-      productDetails
-        .map((product) => product.name)
-        .indexOf(singleProduct.name) !== -1
-    ) {
-      let thatProduct = productDetails.map((product) => product);
-      let thatProductName = thatProduct.map((product) => product.name);
-      let thatProductIndex = thatProductName.indexOf(nameVal);
-      thatProduct[thatProductIndex].count += countVal;
-    } else {
-      productDetails.unshift(singleProduct);
-    }
+    let thatProduct = productDetails.map((product) => product);
+    let thatProductName = thatProduct.map((product) => product.name);
+    let thatProductIndex = thatProductName.indexOf(nameVal);
+    thatProductName.indexOf(singleProduct.name) !== -1
+      ? (thatProduct[thatProductIndex].count += countVal)
+      : productDetails.unshift(singleProduct);
+
     addMsz(added);
     sendToLS(productDetails);
   } else {
-    if (pname.value.length === 0) {
-      borderColorFor(pname);
-    }
-    addMsz(notAdded);
+    pname.value.length === 0 ? borderColorFor(pname) : addMsz(notAdded);
   }
   getFromLS();
   pname.value = "";
   count.value = 1;
 }
-getFromLS("ProductList");
+getFromLS();
 document.getElementById("adder").addEventListener("click", addToLS);
 // sending to localstorage
 function sendToLS(productList) {
@@ -54,36 +46,12 @@ function getFromLS() {
   if (getLS !== null) {
     boxToShow.innerHTML = "";
     LSdata.forEach((element) => {
-      let productHTML = `
-            <div class="col-6 col-sm-4 col-md-2">
-                <button type="button" class="px-4 py-2 rounded-3 text-white bg-primary border-0 fw-bold position-relative" style="letter-spacing: 1px;">
-                  ${element.name}
-                  <span
-                    class="
-                      position-absolute
-                      top-0
-                      start-100
-                      translate-middle
-                      badge
-                      rounded-pill
-                      bg-dark
-                    "
-                  >
-                    ${element.count}
-                    <span class="visually-hidden">unread messages</span>
-                  </span>
-                </button>
-              </div>
-            `;
-      boxToShow.innerHTML = boxToShow.innerHTML + productHTML;
+      let productHTML = `<div class="col-6 col-sm-4 col-md-2"><button type="button" class="px-4 py-2 rounded-3 text-white bg-primary border-0 fw-bold position-relative" style="letter-spacing: 1px;">${element.name}<span class="position-absolute top-0  start-100 translate-middle badge rounded-pill bg-dark">${element.count}<span class="visually-hidden">unread messages</span></span></button></div>`;
+      boxToShow.innerHTML += productHTML;
     });
   } else {
-    boxToShow.innerHTML = `<div class="alert alert-primary" role="alert">
-                                A simple primary alert—check it out!
-                            </div>
-                        `;
+    boxToShow.innerHTML = `<div class="alert alert-primary" role="alert"> A simple primary alert—check it out!</div> `;
   }
-  return LSdata;
 }
 // successFully added
 function addMsz(msz) {
